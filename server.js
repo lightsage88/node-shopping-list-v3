6190f6ff34e060b79c969d0afcd91cbf9b576b45
lightsage88 <adrian.e.rosales@gmail.com>
2017-11-11 14:59:33 -0800

    did it

--- server.js
+++ server.js
@@ -77,6 +77,12 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.delete('/recipes/:id', function(req, res){
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe thing at \`${req.params.id}\` Okie, dokie, its gone now`);
+  res.status(204).end();
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
